8f3865683f1f620d5f102bbee4c216ebc3984e66
Liad Altif <liadaltif@gmail.com>
2025-06-23 18:51:35 +0300

    Refactor server.js to use ES modules and create streams.js for station URLs

--- server.js
+++ server.js
@@ -1,34 +1,15 @@
-const express = require("express");
-const { spawn } = require("child_process");
-const path = require("path");
+import express from "express";
+import { spawn } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+import ffmpegPath from "ffmpeg-static";
+import STREAMS from "./streams.js";
 
 const app = express();
 const PORT = 3000;
 
-
-// Special thanks to the following sources for providing the streams urls:
-// 1. https://www.radio-browser.info/
-// 2. Me, for finding this site...
-
-const STREAMS = {
-
-  "eco99fm": "http://eco-live.mediacast.co.il/99fm_aac",
-  "102fm": "https://102.livecdn.biz/102fm_aac",
-  "103fm": "https://cdn.cybercdn.live/103FM/Live/icecast.audio",
-
-  "radiohaifa": "http://ads11.livecdn.biz:80/radiohaifa",
-  "jerusalemfm": "https://radio.streamgates.net/stream/101fm",
-  "radiodarom":  "https://cdn.cybercdn.live/Darom_97FM/Live/icecast.audio",
-  
-  "levhamedina": "https://cdn.cybercdn.live/Lev_Hamedina/Audio/icecast.audio",
-  "galgalatz": "https://glzwizzlv.bynetcdn.com/glglz_mp3",
-  "galitzahal": "https://glzwizzlv.bynetcdn.com/glz_mp3",
-
-  "kan88": "https://kan88.media.kan.org.il/hls/live/2024812/2024812/kan88_mp3/chunklist.m3u8",
-  "kanbet": "https://kanbet.media.kan.org.il/hls/live/2024811/2024811/kanbet_mp3/chunklist.m3u8",
-  "kangimmel": "https://kangimmel.media.kan.org.il/hls/live/2024813/2024813/kangimmel_mp3/chunklist.m3u8",
-  
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -44,8 +25,6 @@ app.get("/stream/:station", (req, res) => {
     "Cache-Control": "no-cache"
   });
 
-  const ffmpegPath = require("ffmpeg-static");
-  
   const ffmpeg = spawn(ffmpegPath, [
     "-loglevel", "error",
     "-fflags", "nobuffer",
@@ -63,7 +42,6 @@ app.get("/stream/:station", (req, res) => {
     ffmpeg.kill("SIGKILL");
   });
 });
-
 
 app.listen(PORT, () => {
   console.log(`🌐 App running at http://localhost:${PORT}`);
---
+++ streams.js
@@ -0,0 +1,20 @@
+
+// Special thanks to https://www.radio-browser.info/ for providing some of the streams urls
+
+export default {
+  "eco99fm": "http://eco-live.mediacast.co.il/99fm_aac",
+  "102fm": "https://102.livecdn.biz/102fm_aac",
+  "103fm": "https://cdn.cybercdn.live/103FM/Live/icecast.audio",
+
+  "radiohaifa": "http://ads11.livecdn.biz:80/radiohaifa",
+  "jerusalemfm": "https://radio.streamgates.net/stream/101fm",
+  "radiodarom": "https://cdn.cybercdn.live/Darom_97FM/Live/icecast.audio",
+
+  "levhamedina": "https://cdn.cybercdn.live/Lev_Hamedina/Audio/icecast.audio",
+  "galgalatz": "https://glzwizzlv.bynetcdn.com/glglz_mp3",
+  "galitzahal": "https://glzwizzlv.bynetcdn.com/glz_mp3",
+  
+  "kan88": "https://kan88.media.kan.org.il/hls/live/2024812/2024812/kan88_mp3/chunklist.m3u8",
+  "kanbet": "https://kanbet.media.kan.org.il/hls/live/2024811/2024811/kanbet_mp3/chunklist.m3u8",
+  "kangimmel": "https://kangimmel.media.kan.org.il/hls/live/2024813/2024813/kangimmel_mp3/chunklist.m3u8"
+};
